a2b5f3f1d2d3e57c45e6a988c91a4c6d4da6b5cb
kohoutech <george@kohoutech.com>
2020-01-05 23:58:40 -0500

    set up structure of Tidepool source code compiler

---
+++ TidepoolD/Generator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TidepoolD
+{
+    class Generator
+    {
+        public Tidepool tp;
+        public Preprocessor pp;
+
+        public int tpgen_compile()
+        {
+            return 0;
+        }
+
+        public void gexpr() 
+        {
+        }
+
+        public void block()
+        {
+            if (pp.tok.type == TokenType.IF)
+            {
+            }
+            else if (tp.pp.tok.type == TokenType.WHILE)
+            {
+            }
+            else if (tp.pp.tok.type == TokenType.LBRACE)
+            {
+            }
+            else if (tp.pp.tok.type == TokenType.RETURN)
+            {
+            }
+            else if (tp.pp.tok.type == TokenType.BREAK)
+            {
+            }
+            else if (tp.pp.tok.type == TokenType.CONTINUE)
+            {
+            }
+            else if (tp.pp.tok.type == TokenType.FOR)
+            {
+            }
+            else if (tp.pp.tok.type == TokenType.DO)
+            {
+            }
+            else if (tp.pp.tok.type == TokenType.SWITCH)
+            {
+            }
+            if (tp.pp.tok.type == TokenType.CASE)
+            {
+            }
+            else if (tp.pp.tok.type == TokenType.DEFAULT)
+            {
+            }
+            else if (tp.pp.tok.type == TokenType.GOTO)
+            {
+            }
+        }
+    }
+}
---
+++ TidepoolD/Preprocessor.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TidepoolD
+{
+    public class Preprocessor
+    {
+        public Token tok;
+    }
+
+    public class Token
+    {
+        public TokenType type;
+    }
+
+    public enum TokenType
+    {
+        AUTO,
+        BREAK,
+        CASE,
+        CHAR,
+        CONST,
+        CONTINUE,
+        DEFAULT,
+        DO,
+        DOUBLE,
+        ELSE,
+        ENUM,
+        EXTERN,
+        FLOAT,
+        FOR,
+        GOTO,
+        IF,
+        INLINE,
+        INT,
+        LONG,
+        REGISTER,
+        RESTRICT,
+        RETURN,
+        SHORT,
+        SIGNED,
+        SIZEOF,
+        STATIC,
+        STRUCT,
+        SWITCH,
+        TYPEDEF,
+        UNION,
+        UNSIGNED,
+        VOID,
+        VOLATILE,
+        WHILE,
+        
+        LBRACKET,       //[
+        RBRACKET,       //]
+        LPAREN,
+        RPAREN,
+        LBRACE,         //{
+        RBRACE,          //}
+        DOT,
+        ARROW,
+        PLUSPLUS,
+        MINUSMINUS,
+        AMPERSAND,
+        STAR,
+        PLUS,
+        MINUS,
+        TIDLE,
+        EXCLAIM,
+        SLASH,
+        PERCENT,
+        LESSLESS,
+        GTRGTR,
+        LESSTHAN,
+        GTRTHAN,
+        LESSEQU,
+        GTREQU,
+        EQUEQU,
+        NOTEQU,
+        CARET,
+        BAR,
+        AMPAMP,
+        BARBAR,
+        QUESTION,
+        COLON,
+        SEMICOLON,
+        ELIPSIS,
+        COMMA
+
+    }
+}
